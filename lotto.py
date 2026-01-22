8e832226b6d1ed26a5eb21ef2f7f01bd34c9ca39
kobo06042001-del <kobo06042001@gmail.com>
2026-01-22 16:40:36 +0900

    werf

--- lotto.py
+++ lotto.py
@@ -1,19 +1,25 @@
-import streamlit as st
+import streamlit as st  
 import random
 import datetime
 
+
 st.title("로또 번호 생성기")
+
 
 def generate_lotto():
     lotto = set()
     while len(lotto) < 6:
-        number = range(1,46)    # randint면 (1,45)가 맞음
+        number= random.randint(1, 45)
         lotto.add(number)
+
+
     return lotto
 
-button = st.button("로또 번호 생성해주세요!")
+
+button = st.button("로또 번호 생성하기")
+
 
 if button:
-    for i in range(1,6):
-        st.subheader(f"{i}번째 추천 로또 번호: {generate_lotto()}")
-    st.write(f"생성된 시각 : {datetime.datetime.now().strftime('%Y-%M-%d %H:%M:S')}")
+    for i in range(1,6) :
+        st. subheader(f"{i}번째 추천 로또 번호:{generate_lotto()}")
+    st.write(f"생성된 시각 : {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
